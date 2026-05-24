f1e28dcf181f8e1bab43d5f36cf8fba31a48167a
Hydra Driiski <hyoodriiski@users.noreply.github.com>
2026-05-24 10:31:22 +0800

    feat: add main entry point for geoip conversion pipeline

--- main.go
+++ main.go
@@ -1,21 +1,63 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
-	"github.com/spf13/cobra"
+	"github.com/v2fly/geoip/lib"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "geoip",
-	Short: "geoip is a convenient tool to merge, convert and lookup IP & CIDR from various formats of geoip data.",
-	CompletionOptions: cobra.CompletionOptions{
-		HiddenDefaultCmd: true,
-	},
-}
+var (
+	// Version is set at build time via ldflags
+	Version = "dev"
+
+	configFile = flag.String("config", "config.json", "Path to the configuration file")
+	listInput  = flag.Bool("list-input", false, "List all available input formats")
+	listOutput = flag.Bool("list-output", false, "List all available output formats")
+	version    = flag.Bool("version", false, "Print version and exit")
+)
 
 func main() {
-	if err := rootCmd.Execute(); err != nil {
-		log.Fatal(err)
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  geoip [options]\n\n")
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
 	}
+
+	flag.Parse()
+
+	if *version {
+		fmt.Printf("geoip version %s\n", Version)
+		os.Exit(0)
+	}
+
+	if *listInput {
+		lib.ListInputFormats()
+		os.Exit(0)
+	}
+
+	if *listOutput {
+		lib.ListOutputFormats()
+		os.Exit(0)
+	}
+
+	// Load and validate the configuration file
+	instance, err := lib.NewInstance()
+	if err != nil {
+		log.Fatalf("failed to create instance: %v", err)
+	}
+
+	if err := instance.InitConfig(*configFile); err != nil {
+		log.Fatalf("failed to initialize config from %q: %v", *configFile, err)
+	}
+
+	// Run the conversion pipeline
+	if err := instance.Run(); err != nil {
+		log.Fatalf("failed to run: %v", err)
+	}
+
+	log.Println("done")
 }
